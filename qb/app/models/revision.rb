92e39e99a8a35437f1d6eaee084855b5041e6268
grenzi <grenzi@5e073825-67f1-0310-ba69-ae2cb73c4416>
2005-11-23 23:32:25 +0000

    oops forgote to rename files in version->revision transition

---
+++ qb/app/models/revision.rb
@@ -0,0 +1,50 @@
+class Revision < ActiveRecord::Base
+  PAGE_LINK = /\[\[([^\]|]*)[|]?([^\]]*)\]\]/
+  
+  belongs_to :page
+  belongs_to :author
+ # after_save  :page_was_updated
+  validates_presence_of :author,:body
+  validates_associated :author
+
+  def page_links
+    @page_links ||= body.scan(PAGE_LINK).flatten.reject { |link| link.empty? }
+  end
+
+  def newer_revisions
+    @newer_revisions ||= revisions[revision_index + 1..-1]
+  end
+
+  def newer_revisions?
+    !newer_revisions.empty?
+  end
+
+  def older_revisions
+    @older_revisions ||= revisions[0...revision_index].reverse
+  end
+  
+  def older_revisions?
+    !older_revisions.empty?
+  end
+  
+  def previous_revision
+    @previous_revision ||= older_revisions.first
+  end
+
+  def revisions
+    page.revisions
+  end
+
+  def number
+    revision_index + 1
+  end
+
+  protected
+    def revision_index
+      revisions.index(self)
+    end
+    
+    def page_was_updated
+      page.save
+    end
+end
---
+++ qb/test/functional/revisions_controller_test.rb
@@ -0,0 +1,75 @@
+require File.dirname(__FILE__) + '/../test_helper'
+require 'revisions_controller'
+
+# Re-raise errors caught by the controller.
+class RevisionsController; def rescue_action(e) raise e end; end
+
+class RevisionsControllerTest < Test::Unit::TestCase
+  fixtures :pages, :revisions, :authors
+  
+  def setup
+    @controller = RevisionsController.new  
+    @request    = ActionController::TestRequest.new
+    @response   = ActionController::TestResponse.new
+  end
+
+
+  def test_create_existing_author
+    assert_nil Page.find_by_title("A brave new world")
+    authorname='David'
+    post(
+      :create, 
+      {
+        :page => { :title => "A brave new world" }, 
+        :revision => { :body => "So wonderful!" }
+      },
+      { #session vars
+        "authenticated" => true,
+        "author_name" =>   authorname
+      },
+      {# flashvars }
+      }    
+    )
+    assert_equal session['author_name'],authorname
+    assert_response :redirect
+
+    page=Page.find_by_title("A brave new world")
+    assert_not_nil page, "page nil, it was not saved"
+    assert_not_nil  page.current_revision  , "revision nil it was not saved"
+
+    assert_equal "So wonderful!", page.revisions.first.body
+    assert_equal authors(:david), page.revisions.first.author
+  end
+
+
+  def test_create_new_author
+    assert_nil Page.find_by_title("A brave new world")
+
+    authorname="Some new AuthorName"
+    post(
+      :create, 
+      { #params
+        :page => { :title => "A brave new world" }, 
+        :revision => { :body => "So wonderful!" }
+      },
+      { #session vars
+        "authenticated" => true,
+        "author_name" =>   authorname
+      },
+      {# flashvars }
+      }    
+    )
+    assert_response :redirect
+    assert_equal session['author_name'],authorname
+    
+    page=Page.find_by_title("A brave new world")
+    author= Author.find_by_name  authorname
+    
+    assert_not_nil page
+    assert_not_nil author
+    assert_not_nil page.current_revision  
+    
+    assert_equal "So wonderful!", page.revisions.first.body
+    assert_equal author, page.revisions.first.author
+  end
+end
