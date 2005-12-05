08c85e420b1e894b026d3bf5897ac01da2bfb29a
grenzi <grenzi@5e073825-67f1-0310-ba69-ae2cb73c4416>
2005-12-05 03:26:28 +0000

    removed more uselss stuff

--- qb/app/controllers/application.rb
+++ qb/app/controllers/application.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_filter :set_charset
-
-
-  protected
-  def set_charset
-    @headers["Content-Type"] = "text/html; charset=utf-8" 
-  end
 end
